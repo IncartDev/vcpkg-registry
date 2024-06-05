b1ae240c17936a1f98ad999ae6488527da0c03ee
Aleksey N. Mihaylov <man@incart.ru>
2024-06-05 16:17:56 +0300

    debugging fileunv

--- ports/fileunv/portfile.cmake
+++ ports/fileunv/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git@github.com:IncartDev/crossplatform-file-dll.git
-    REF d0c7b43bd8af18001d4fa454c14bd2d404ead04a
+    REF a60c8bc12e620385b887fc567fdaab48423229b9
     HEAD_REF man
 )
 
